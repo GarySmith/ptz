6ae9b0f19adc448d5ff0f9fa9a6791e038b917bb
Gary Smith <gary.smith@suse.com>
2018-06-29 15:27:29 -0700

    Integrate with server
    Fetch the camera presets from the server at page load and draw on the
    screen.

--- src/App.js
+++ src/App.js
@@ -6,24 +6,53 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state={
-      expanded:false
+      expanded: false,
+      presets : []
     };
   }
+
+  componentDidMount = () => {
+    const init = {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json',
+      }
+    };
+
+    fetch('/api/presets', init)
+    .then(response => response.json())
+    .then(response => {
+      this.setState({presets: response});
+    });
+  }
+
   render() {
     let menuclass="sidenav";
     if(this.state.expanded) {
       menuclass+=" expanded";
     }
+    const buttons = this.state.presets.map(e => (
+      <div key={e.num}>
+        {e.num}
+      </div>
+    ));
+
     return (
       <div>
-        <div key="sideId" className={menuclass}>
-          <a href="javascript:void(0)" className="closebtn" onClick={() => this.closeNav()}> &times;</a>
-          <a href="#">Name</a>
-          <a href="#">Address</a>
-          <a href="#">Calibrate</a>
-          <a href="#">Update/Upload Image</a>
+        <div>
+          <div key="sideId" className={menuclass}>
+            <a href="javascript:void(0)" className="closebtn" onClick={() => this.closeNav()}> &times;</a>
+            <a href="#">Name</a>
+            <a href="#">Address</a>
+            <a href="#">Calibrate</a>
+            <a href="#">Update/Upload Image</a>
+          </div>
+          <span onClick={(e) => this.openNav(e)}>&#9776; menu</span>
         </div>
-        <span onClick={(e) => this.openNav(e)}>&#9776; menu</span>
+        <div className="content">
+          {buttons}
+        </div>
       </div>
     );
   }
