91695625edb49a8be978d41d6de23970f6b60eea
Holly Donis <hollysmithrocklin@gmail.com>
2018-06-30 14:26:02 -0700

    first version of nav menu

--- src/App.js
+++ src/App.js
@@ -3,18 +3,35 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      expanded:false
+    };
+  }
   render() {
+    let menuclass="sidenav";
+    if(this.state.expanded) {
+      menuclass+=" expanded";
+    }
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <div key="sideId" className={menuclass}>
+          <a href="javascript:void(0)" className="closebtn" onClick={() => this.closeNav()}> &times;</a>
+          <a href="#">Name</a>
+          <a href="#">Address</a>
+          <a href="#">Calibrate</a>
+          <a href="#">Update/Upload Image</a>
+        </div>
+        <span onClick={(e) => this.openNav(e)}>&#9776; menu</span>
       </div>
     );
+  }
+  closeNav(e) {
+    this.setState({expanded: false});
+  }
+  openNav() {
+    this.setState({expanded: true});
   }
 }
 
