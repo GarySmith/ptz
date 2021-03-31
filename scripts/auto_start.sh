bdac7e5bc518b645c2ca91a19e41728e44a1f923
Hope Lutheran Church <hopepenryn@gmail.com>
2021-03-30 23:24:48 -0700

    Automatically start recording if laptop started right before a known service time

--- scripts/auto_start.sh
+++ scripts/auto_start.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-# Detect whether we are inside or outside based on the presence
-# of the wired interface IP address
-if ip -o addr show enp3s0 | grep -q inet ; then
-    # We have a LAN connection -- start the church VLC
-    /usr/bin/vlc rtsp://192.168.0.88:554/2
+# Only do the auto-start when at Hope, i.e. the wireless address is 192.168.1.x
+
+if ip -o addr show up wlo1 | grep 'inet ' | grep -q 192.168.1\\. ; then
+
+    # Detect whether we are inside or outside based on the presence
+    # of the wired interface IP address
+    if ip -o addr show enp3s0 | grep -q inet ; then
+        # We have a LAN connection -- start the church VLC
+        /usr/bin/vlc rtsp://192.168.0.88:554/2 &
+        /data/ptz/scripts/control_vlc setup
+    else
+        /data/ptz/scripts/control_vlc setup
+        /usr/bin/python3 /data/ptz/scripts/start_vlc.py
+    fi
+
 else
-    /usr/bin/python3 /data/ptz/scripts/start_vlc.py
+    /usr/bin/vlc 'v4l2:///dev/video0::live-caching=300' &
+    /data/ptz/scripts/control_vlc setup
 fi
